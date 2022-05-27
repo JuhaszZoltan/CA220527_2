90f28be8281d5bc4c6e87d5585011bbb06f97079
JuhaszZoltan <JuhaszZoltanMate@yahoo.com>
2022-05-27 18:37:31 +0200

    Add project files.

---
+++ Film.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA220527_2
+{
+    internal class Film
+    {
+        //prop + TAB + TAB
+        public string Cim { get; set; }
+        public int Fazis { get; set; }
+        public string Premier { get; set; }
+        public int Kiadas { get; set; }
+        public int Bevetel { get; set; }
+
+        public float BKArany => Bevetel / (float)Kiadas;
+        public int Haszon => Bevetel - Kiadas;
+
+
+
+        //ctor + TAB + TAB
+
+        public Film(string sor)
+        {
+            var v = sor.Split(';');
+
+            Cim = v[0];
+            Fazis = int.Parse(v[1]);
+            Premier = v[2];
+            Kiadas = int.Parse(v[3]);
+            Bevetel = int.Parse(v[4]);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using CA220527_2;
+
+List<Film> filmek = new();
+
+using StreamReader sr = new(@"../../../res/marvel.txt");
+
+while (!sr.EndOfStream)
+    filmek.Add(new Film(sr.ReadLine()));
+
+//"A" feladatsor 3AB.1es feladat:
+Console.WriteLine($"3A.1: {filmek.Count} film van a marvel moziverzumában");
+
+//"A" feladatsor 3A.2es feladat:
+int koltsegSum = 0;
+foreach (var film in filmek)
+{
+    koltsegSum += film.Kiadas;
+}
+float kotsegAvg = koltsegSum / (float)filmek.Count;
+Console.WriteLine($"3A.2: a filmek átlagos gyártási költsége: ${kotsegAvg : 0.00}M");
+
+//"A" feladatsor 3A.3as feladat:
+int maxIndex = 0;
+for (int i = 1; i < filmek.Count; i++)
+{
+    if (filmek[i].BKArany > filmek[maxIndex].BKArany)
+    {
+        maxIndex = i;
+    }
+}
+Console.WriteLine($"3A.3: a legkötslghatékonyabb film címe: {filmek[maxIndex].Cim}");
+
+//"A" feladatsor 3A.4es feladat:
+Console.Write("3A.4: írjon be egy évszámot: ");
+string keresettEv = Console.ReadLine();
+Console.WriteLine($"a {keresettEv}-ban/ben megjelent marvel filmek:");
+//fore + TAB + TAB
+foreach (var film in filmek)
+{
+    if (film.Premier.Contains(keresettEv))
+    {
+        Console.WriteLine($"\t{film.Cim}");
+    }
+}
+
+//------------------------------------------------
+
+//"B" feladatsor 3B.2es feladat:
+int fazisCount = 0;
+foreach (var film in filmek)
+{
+    if (film.Fazis < 3) fazisCount++;
+}
+Console.WriteLine($"3B.2: A 3. fázis előtti filmek száma: {fazisCount}db");
+
+//"B" feladatsor 3B.3as feladat:
+int minIndex = 0;
+for (int i = 1; i < filmek.Count; i++)
+{
+    if (filmek[i].Haszon < filmek[minIndex].Haszon)
+    {
+        minIndex = i;
+    }       
+}
+Console.WriteLine($"3B.3: A legalacsonyabb hasznot a(z) {filmek[minIndex].Cim} hozta!");
+
+//"B" feladat 3B.4es feladat:
+Console.Write("3B.4: írjon be egy címrészletet: ");
+string cimReszlet = Console.ReadLine();
+foreach (var film in filmek)
+{
+    if (film.Cim.Contains(cimReszlet))
+    {
+        Console.WriteLine($"\t{film.Cim}");
+    }
+}
+
+Console.ReadKey(true);
